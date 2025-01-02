331a441a74f3baee21abf8dcd47081046ff315a1
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-01-02 13:43:57 +0800

    Update generate_and_send_email.py

--- generate_and_send_email.py
+++ generate_and_send_email.py
@@ -9,7 +9,6 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-# Function to clean response text
 def clean_response_text(text):
     text = re.sub(r'\\', '', text)
     return text
